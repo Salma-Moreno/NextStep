ad0866cd8ff941a7afd9248bdb185b5e284fa067
Salma Moreno <l22211911@tectijuana.edu.mx>
2025-11-24 00:35:17 -0800

    Merequetenge en los puntos de entrega(mapa sin terminar)

--- Conexiones/db.php
+++ Conexiones/db.php
@@ -1,7 +1,7 @@
 <?php
 $host = '127.0.0.1';
-// $port = 3306; // Puerto determinado de XAMPP, cambienlo si lo necesitan.
-$port = 3307; // Este es el puerto que yo uso, porfa no lo borren 
+$port = 3306; // Puerto determinado de XAMPP, cambienlo si lo necesitan.
+//$port = 3307; // Este es el puerto que yo uso, porfa no lo borren 
 $dbname = 'nextstepdatabase';
 $username = 'root'; // Cambiar si tu servidor tiene otro usuario
 $password = ''; // Si tu usuario tiene contraseña, cámbiala
